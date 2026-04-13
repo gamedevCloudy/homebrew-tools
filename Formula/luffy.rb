69c3d30c77267ba1abc84dd270fd92acab722e77
DemonKingSwarn <rockingswarn@gmail.com>
2026-04-13 12:27:21 +0530

    chore: windows patch

--- Formula/luffy.rb
+++ Formula/luffy.rb
@@ -20,12 +20,12 @@ class Luffy < Formula
 
   on_arm do
     url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.4/luffy-darwin-arm64"
-    sha256 "e60d0bbd53f9c22637aff3f5abe7e2422b52ab1661b47d6a8614239a63611a16"
+    sha256 "aa1bb8e88cedabc4f26a71bddf6648f0abc313d73a9d76d7e8e406e619d1be31"
   end
 
   on_intel do
     url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.4/luffy-darwin-amd64"
-    sha256 "70f258c3f1109e6bea691a034794ba23e7cf68cf115f056a66ada4ffb32015a3"
+    sha256 "8565340574e32ffc409aaeed4ebb0bcc0d432bb6018be0e59e4ca88e46cd02c2"
   end
 
 
