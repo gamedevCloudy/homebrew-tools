298a08f2aa8c10c4a4e4a52812aded2400c0077f
DemonKingSwarn <rockingswarn@gmail.com>
2026-04-13 12:06:42 +0530

    chore: added ability to change player to mpv for macos

--- Formula/luffy.rb
+++ Formula/luffy.rb
@@ -19,13 +19,13 @@ class Luffy < Formula
   end
 
   on_arm do
-    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.1/luffy-darwin-arm64"
-    sha256 "9bcc0dabdd89a97e648acb2e064a6b8718e4be0e672ecbf1ced32350b9e91601"
+    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.4/luffy-darwin-arm64"
+    sha256 "c654972cc4fa8718aa1d78e65ba951486c12dd33226d148d8f11afeb445e872c"
   end
 
   on_intel do
-    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.1/luffy-darwin-amd64"
-    sha256 "2577475f2b7b142e439959a076847415b06170dd96b0e0961e990f4d2ecf999f"
+    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.4/luffy-darwin-amd64"
+    sha256 "3d3ccb93c02d89e4abcf7b9b074bcbe4b4442566fdf37c8fda70f2fc24dfae2f"
   end
 
 
