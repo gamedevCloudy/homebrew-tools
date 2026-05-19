6b9887b6b430661e399bad0734037160a691b815
DemonKingSwarn <swarnaditya.7@proton.me>
2026-05-19 16:44:18 +0530

    chore: luffy update

--- Formula/luffy.rb
+++ Formula/luffy.rb
@@ -19,13 +19,13 @@ class Luffy < Formula
   end
 
   on_arm do
-    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.5/luffy-darwin-arm64"
-    sha256 "6b495337330aad2ed609d609f6abbac98800e142f3699ae56c32236fbe8d896e"
+    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.2.0/luffy-darwin-arm64"
+    sha256 "79a0b0a12b72d3778a9575ce6a4e4cf2c036611353f6d171e8b670652c8b19cd"
   end
 
   on_intel do
-    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.1.5/luffy-darwin-amd64"
-    sha256 "c23a8877d96be0a9064c0d89aa95d2b9cdb24f967e71b7731a65fe3af620b0e7"
+    url "https://github.com/DemonKingSwarn/luffy/releases/download/v1.2.0/luffy-darwin-amd64"
+    sha256 "82d86154cc2f0d9ba59cdaf17109a57e6e5c4cf5375f22a457467f2d5bddc8ac"
   end
 
 
